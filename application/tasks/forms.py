4f43688103d012a91f4af24bf9491420ace448b0
Hanna Vuorivirta <hannavuorivirta@gmail.com>
2020-04-23 17:46:23 +0300

    add kayttotapaukset.txt

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -2,8 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, validators
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=2)])
-    done = BooleanField("Done")
+    name = StringField("Tehtävän nimi", [validators.Length(min=2)])
+    done = BooleanField("Tehty")
   
     class Meta:
         csrf = False
+
+'''class ShoppingForm(FlaskForm):
+    name = StringField("Lisää ostoslistalle", [validators.Length(min=2)])
+    done = BooleanField("Ostettu")
+
+    class Meta:
+        csrf = False'''
